232afd14e70a03d3eb155a477f5c097c4d73bb25
Lasse Einfeldt <lasseeinfeldt@gmail.com>
2016-11-08 11:32:59 +0100

    Set up the route and send the html file

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+/**
+ * @desc Import libraries and create an instance of express
+ */
+var express = require('express');
+var app = express();
+
+
+//_____________________
+// CONNECT TO DB
+
+/**
+ * @desc retreive data from the frontend and store it in the DB
+ */
+app.post('/saveGPS', function(req, res) {
+    // store the data in the DB
+})
+
