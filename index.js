7447cab3b520ad9021cd4ade0f04cf839ac6c98e
Lasse Einfeldt <lasseeinfeldt@gmail.com>
2016-11-15 11:43:13 +0100

    add connection to db in node + gitignore

--- index.js
+++ index.js
@@ -4,12 +4,41 @@
 /**
  * @desc Import libraries and create an instance of express
  */
+var config = require('./config.js');
 var express = require('express');
+var pg = require('pg');
 var app = express();
+var client = new pg.Client();
 
 
-//_____________________
-// CONNECT TO DB
+/**
+ * @desc Setup the client pool
+ */
+var pool = new pg.Pool(config.dbdata);
+
+
+/**
+ * @desc Connect to the database
+ */
+pool.connect(function(err, client, done) {
+    if (err) throw err;
+
+    //execute an operation
+    client.query(...someQuery...), function(err, result) {
+        //release client to pool
+        done();
+
+        if(err) {
+            return console.error('error in query', err);
+        }
+        console.log(result.rows[0].numbers);
+        // do some stuff with the output
+    }
+});
+
+pool.on('error', function(err, client) {
+    console.error('idle client error', err.message, err.stack);
+});
 
 /**
  * @desc retreive data from the frontend and store it in the DB
